e99c1fa27303fa0d0e99647367e1e17ef3a6f9df
Ivar Carrera <ivar.carrera@outlook.com>
2023-06-07 10:13:36 -0300

    Initial Project Structure

---
+++ src/config.py
@@ -0,0 +1,17 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+ENV: str = os.getenv('ENV', 'production').lower()
+if ENV not in ('production', 'development', 'testing'):
+    raise ValueError(
+        f'ENV={ENV} is not valid. '
+        "It should be 'production', 'development' or 'testing'"
+    )
+DEBUG: bool = ENV != 'production'
+TESTING: bool = ENV == 'testing'
+
+LOG_LEVEL: str = os.getenv('LOG_LEVEL') or (DEBUG and 'DEBUG') or 'INFO'
+os.environ['LOGURU_DEBUG_COLOR'] = '<fg #777>'
---
+++ src/main.py
@@ -0,0 +1,4 @@
+ENV=testing
+DB_PASSWORD=firework
+DB_PORT=5431
+DB_NAME=currency_converter
