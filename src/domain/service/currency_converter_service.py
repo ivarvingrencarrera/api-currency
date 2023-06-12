24d0de2d2c0af4984a233208edcefa984a6fdc52
Ivar Carrera <ivar.carrera@outlook.com>
2023-06-12 11:33:31 -0300

    feat: Create a validation to negative or zero amount in convert function

--- src/domain/service/currency_converter_service.py
+++ src/domain/service/currency_converter_service.py
@@ -7,4 +7,7 @@ from src.domain.entity.exchange_rate import ExchangeRate
 class CurrencyConverterService(ABC):
     @abstractmethod
     def convert(exchange_rates: ExchangeRate, from_currency: Currency, to_currency: Currency, amount: float) -> float:
+        if amount <= 0:
+            raise ValueError('Amount must be greater than zero')
         return round(amount / exchange_rates.rate, 2)
+        
