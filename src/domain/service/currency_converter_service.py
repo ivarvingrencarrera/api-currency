ba591ed08d8b9f5020019d12b27223dc8409c397
Ivar Carrera <ivar.carrera@outlook.com>
2023-06-12 12:11:48 -0300

    feat: Create a format_currency function

--- src/domain/service/currency_converter_service.py
+++ src/domain/service/currency_converter_service.py
@@ -6,8 +6,14 @@ from src.domain.entity.exchange_rate import ExchangeRate
 
 class CurrencyConverterService(ABC):
     @abstractmethod
-    def convert(exchange_rates: ExchangeRate, from_currency: Currency, to_currency: Currency, amount: float) -> float:
+    def convert(exchange_rate: ExchangeRate, amount: float) -> float:
         if amount <= 0:
             raise ValueError('Amount must be greater than zero')
-        return round(amount / exchange_rates.rate, 2)
-        
+        return round(amount / exchange_rate.rate, 2)
+
+    @abstractmethod
+    def format_currency(amount: float, currency: Currency) -> str:
+        integer_value, decimal_value = divmod(amount, 1)
+        integer_formatted = f'{integer_value:,.0f}'.replace(',', '.')
+        decimal_formatted = f'{int(min(decimal_value, 0.99) * 100):02d}'
+        return f'{currency.symbol}{integer_formatted},{decimal_formatted}'
