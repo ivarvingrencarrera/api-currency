3aca4db6d7b0f1cc3c55f5e5c73ddc8e7c7fd57e
Ivar Carrera <ivar.carrera@outlook.com>
2023-06-12 11:26:44 -0300

    text: Create a unit test case CurrencyConverterServiceTests

---
+++ test/unit/test_currency_converter_service.py
@@ -0,0 +1,24 @@
+import unittest
+from datetime import datetime
+
+from parameterized import parameterized
+
+from src.domain.entity.currency import Currency
+from src.domain.entity.exchange_rate import ExchangeRate
+from src.domain.service.currency_converter_service import CurrencyConverterService
+
+
+class CurrencyConverterServiceTests(unittest.TestCase):
+    def setUp(self) -> None:
+        self.from_currency = Currency(1, 'BRL', 986, 'Brazilian Real', 'R$')
+        self.to_currency = Currency(2, 'EUR', 978, 'Euro', '€')
+        self.exchange_rate = ExchangeRate(5687, self.from_currency.id, self.to_currency.id, datetime.now(), 4.76)
+
+    @parameterized.expand([(100, 21.01), (200, 42.02)])
+    def test_must_converter_the_amount(self, input_: float, expected_output: float) -> None:
+        output = CurrencyConverterService.convert(self.exchange_rate, self.from_currency, self.to_currency, input_)
+        self.assertEqual(output, expected_output)
+
+
+if __name__ == '__main__':
+    unittest.main()
