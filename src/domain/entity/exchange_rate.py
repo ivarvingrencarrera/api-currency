11d675481abd791c92f0db6a0f0251986c38f129
Ivar Carrera <ivar.carrera@outlook.com>
2023-06-12 10:48:58 -0300

    feat: Create a validate_date function

--- src/domain/entity/exchange_rate.py
+++ src/domain/entity/exchange_rate.py
@@ -1,9 +1,10 @@
-from datetime import datetime
+from datetime import datetime, date
 
 
 class ExchangeRate:
     def __init__(self, id: int, from_currency_id: int, to_currency_id: int, date: datetime, rate: float):
         self.validate_rate(rate)
+        self.validate_date(date)
         self.id = id
         self.from_currency = from_currency_id
         self.to_currency = to_currency_id
@@ -14,3 +15,8 @@ class ExchangeRate:
     def validate_rate(rate: float) -> None:
         if rate < 0:
             raise ValueError('Rate must not be negative')
+
+    @staticmethod
+    def validate_date(date_datetime: datetime) -> None:
+        if date_datetime.date() != date.today():
+            raise ValueError('The date is not the current date')
