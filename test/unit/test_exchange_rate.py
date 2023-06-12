8409bf7128f325957b020d27b3cf78a32548428c
Ivar Carrera <ivar.carrera@outlook.com>
2023-06-12 11:01:15 -0300

    style: Break line in test_must_create_exchange_rate function

--- test/unit/test_exchange_rate.py
+++ test/unit/test_exchange_rate.py
@@ -13,14 +13,16 @@ class ExchangeRateTests(unittest.TestCase):
         self.rate = 0.85
 
     def test_must_create_exchange_rate(self) -> None:
-        exchange_rate = ExchangeRate(self.id, self.from_currency_id, self.to_currency_id, self.date, self.rate)
+        exchange_rate = ExchangeRate(
+            self.id, self.from_currency_id, self.to_currency_id, self.date, self.rate
+        )
         self.assertEqual(exchange_rate.rate, 0.85)
 
     def test_must_not_create_exchange_rate_with_negative_rate(self) -> None:
         with self.assertRaises(ValueError) as context:
             ExchangeRate(self.id, self.from_currency_id, self.to_currency_id, self.date, -0.85)
             self.assertTrue('Rate must not be negative' in str(context.exception))
-    
+
     def test_must_not_create_exchange_rate_with_date_out_of_date(self) -> None:
         date_string = '2023-06-09 00:00:00'
         date = datetime.strptime(date_string, '%Y-%m-%d %H:%M:%S')
