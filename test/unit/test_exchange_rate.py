ce0a21cd6d98daa0e7e60cfc5701cffa20f91f0a
Ivar Carrera <ivar.carrera@outlook.com>
2023-06-12 10:52:03 -0300

    test: Create test_must_not_create_exchange_rate_with_date_out_of_date

--- test/unit/test_exchange_rate.py
+++ test/unit/test_exchange_rate.py
@@ -20,6 +20,13 @@ class ExchangeRateTests(unittest.TestCase):
         with self.assertRaises(ValueError) as context:
             ExchangeRate(self.id, self.from_currency_id, self.to_currency_id, self.date, -0.85)
             self.assertTrue('Rate must not be negative' in str(context.exception))
+    
+    def test_must_not_create_exchange_rate_with_date_out_of_date(self) -> None:
+        date_string = '2023-06-09 00:00:00'
+        date = datetime.strptime(date_string, '%Y-%m-%d %H:%M:%S')
+        with self.assertRaises(ValueError) as context:
+            ExchangeRate(self.id, self.from_currency_id, self.to_currency_id, date, self.rate)
+            self.assertTrue('The date is not the current date' in str(context.exception))
 
 
 if __name__ == '__main__':
