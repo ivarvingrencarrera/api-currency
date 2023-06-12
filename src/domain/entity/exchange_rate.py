ef96ab32f324274c3f4fc29f9a37a44ffb1f0d0c
Ivar Carrera <ivar.carrera@outlook.com>
2023-06-12 10:43:30 -0300

    feat: Create a validate_rate function

--- src/domain/entity/exchange_rate.py
+++ src/domain/entity/exchange_rate.py
@@ -3,8 +3,14 @@ from datetime import datetime
 
 class ExchangeRate:
     def __init__(self, id: int, from_currency_id: int, to_currency_id: int, date: datetime, rate: float):
+        self.validate_rate(rate)
         self.id = id
         self.from_currency = from_currency_id
         self.to_currency = to_currency_id
         self.date = date
         self.rate = rate
+
+    @staticmethod
+    def validate_rate(rate: float) -> None:
+        if rate < 0:
+            raise ValueError('Rate must not be negative')
