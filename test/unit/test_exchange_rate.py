7c45bd38adaabe6c3678e55721be5ae63e8d18d1
Ivar Carrera <ivar.carrera@outlook.com>
2023-06-12 10:44:44 -0300

    test: Create test_must_not_create_exchange_rate_with_negative_rate

--- test/unit/test_exchange_rate.py
+++ test/unit/test_exchange_rate.py
@@ -13,8 +13,13 @@ class ExchangeRateTests(unittest.TestCase):
         self.rate = 0.85
 
     def test_must_create_exchange_rate(self) -> None:
-        exchange_rate = ExchangeRate(5687, 1, 2, self.date, self.rate)
+        exchange_rate = ExchangeRate(self.id, self.from_currency_id, self.to_currency_id, self.date, self.rate)
         self.assertEqual(exchange_rate.rate, 0.85)
+
+    def test_must_not_create_exchange_rate_with_negative_rate(self) -> None:
+        with self.assertRaises(ValueError) as context:
+            ExchangeRate(self.id, self.from_currency_id, self.to_currency_id, self.date, -0.85)
+            self.assertTrue('Rate must not be negative' in str(context.exception))
 
 
 if __name__ == '__main__':
