46a47305aab603c1bcf82655e06e778638c42def
Ivar Carrera <ivar.carrera@outlook.com>
2023-06-13 11:20:30 -0300

    feat: Create a find function

--- src/application/repository/exchange_rate_repository.py
+++ src/application/repository/exchange_rate_repository.py
@@ -1,9 +1,10 @@
 from abc import abstractmethod
+from datetime import datetime
 
 from src.domain.entity.exchange_rate import ExchangeRate
 
 
 class ExchangeRateRepository:
     @abstractmethod
-    def get_rate(self, from_currency_id: int, to_currency_id: int, date: str) -> ExchangeRate:
+    async def find(self, from_currency_id: int, to_currency_id: int, date: datetime) -> ExchangeRate:
         pass   # pragma: no cover
--- src/infrastructure/repository/exchange_rate_repository_database.py
+++ src/infrastructure/repository/exchange_rate_repository_database.py
@@ -1,12 +1,28 @@
-from typing import Any
+from datetime import datetime
 
 from src.application.repository.exchange_rate_repository import ExchangeRateRepository
 from src.domain.entity.exchange_rate import ExchangeRate
+from src.infrastructure.database.connection import Connection
 
 
 class ExchangeRateRepositoryDatabase(ExchangeRateRepository):
-    def __init__(self, database: Any) -> None:
-        self.database = database
+    def __init__(self, connection: Connection) -> None:
+        self.connection = connection
 
-    def get_rate(self, from_currency_id: int, to_currency_id: int, date: str) -> ExchangeRate:
-        return self.database.get_exchange_rate(from_currency_id, to_currency_id, date)
+    async def find(self, from_currency_id: int, to_currency_id: int, date: datetime) -> ExchangeRate:
+        exchange_rate_query: str = ' \
+            SELECT * FROM converter.exchange_rate \
+            WHERE from_currency_id = $1 AND to_currency_id = $2 AND DATE(date_rate) = $3;'
+        exchange_rate_data: list = await self.connection.select(
+            exchange_rate_query, from_currency_id, to_currency_id, date
+        )
+        if not exchange_rate_data:
+            raise ValueError('No exchange rate found for the given currencies.')
+        exchange_rate = exchange_rate_data[0]
+        return ExchangeRate(
+            exchange_rate.id,
+            exchange_rate.from_currency_id,
+            exchange_rate.to_currency_id,
+            exchange_rate.date_rate,
+            float(exchange_rate.rate),
+        )
