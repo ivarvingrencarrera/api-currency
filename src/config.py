1814fcd0fbad8f55ad1e61a129ddfc97b0901981
Ivar Carrera <ivar.carrera@outlook.com>
2023-06-13 11:39:04 -0300

    build: Add database configs

--- src/config.py
+++ src/config.py
@@ -12,3 +12,10 @@ TESTING: bool = ENV == 'testing'
 
 LOG_LEVEL: str = os.getenv('LOG_LEVEL') or (DEBUG and 'DEBUG') or 'INFO'
 os.environ['LOGURU_DEBUG_COLOR'] = '<fg #777>'
+
+DB_HOST = os.environ['DB_HOST']
+DB_PORT = os.environ['DB_PORT']
+DB_NAME = os.environ['DB_NAME']
+DB_USER = os.environ['DB_USER']
+DB_PASSWORD = os.environ['DB_PASSWORD']
+DATABASE_URL = f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
