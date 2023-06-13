31810893f948f3fb0740ff845e75d65dfcb71a25
Ivar Carrera <ivar.carrera@outlook.com>
2023-06-13 11:19:33 -0300

    feat: Create a find_by_code function

--- src/application/repository/currency_repository.py
+++ src/application/repository/currency_repository.py
@@ -5,5 +5,5 @@ from src.domain.entity.currency import Currency
 
 class CurrencyRepository:
     @abstractmethod
-    def get_currency_by_code(self, code: str) -> Currency:
+    async def find_by_code(self, code: str) -> Currency:
         pass   # pragma: no cover
--- src/infrastructure/repository/currency_repository_database.py
+++ src/infrastructure/repository/currency_repository_database.py
@@ -1,13 +1,22 @@
-from typing import Any
-
 from src.application.repository.currency_repository import CurrencyRepository
 from src.domain.entity.currency import Currency
+from src.infrastructure.database.connection import Connection
 
 
 class CurrencyRepositoryDatabase(CurrencyRepository):
-    def __init__(self, database: Any) -> None:
-        self.database = database
+    def __init__(self, connection: Connection) -> None:
+        self.connection = connection
 
-    def get_currency_by_code(self, code: str) -> Currency:
-        currency = self.database.get_currency_by_code(code)
-        return Currency(**currency)
+    async def find_by_code(self, code: str) -> Currency:
+        currency_query: str = 'SELECT * FROM converter.currency WHERE alphabetic_code = $1;'
+        currency_data: list = await self.connection.select(currency_query, code)
+        if not currency_data:
+            raise ValueError(f'Currency with code {code} not found')
+        currency = currency_data[0]
+        return Currency(
+            currency.id,
+            currency.alphabetic_code,
+            currency.numeric_code,
+            currency.name,
+            currency.symbol,
+        )
