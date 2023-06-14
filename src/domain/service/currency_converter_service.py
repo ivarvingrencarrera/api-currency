807040fa9a37cd90a23b4f079657d061e5d5433e
Ivar Carrera <ivar.carrera@outlook.com>
2023-06-14 18:51:14 -0300

    refactor: Update variable name

--- src/domain/service/currency_converter_service.py
+++ src/domain/service/currency_converter_service.py
@@ -4,14 +4,14 @@ from src.domain.entity.exchange_rate import ExchangeRate
 
 class CurrencyConverterService:
     @staticmethod
-    def convert(exchange_rate: ExchangeRate, amount: float) -> float:
-        if amount <= 0:
-            raise ValueError('Amount must be greater than zero')
-        return round(amount / exchange_rate.rate, 2)
+    def convert(exchange_rate: ExchangeRate, value: float) -> float:
+        if value <= 0:
+            raise ValueError('value must be greater than zero')
+        return round(value / exchange_rate.rate, 2)
 
     @staticmethod
-    def format_currency(amount: float, currency: Currency) -> str:
-        integer_value, decimal_value = divmod(amount, 1)
+    def format_currency(value: float, currency: Currency) -> str:
+        integer_value, decimal_value = divmod(value, 1)
         integer_formatted = f'{integer_value:,.0f}'.replace(',', '.')
         decimal_formatted = f'{int(min(decimal_value, 0.99) * 100):02d}'
         return f'{currency.symbol}{integer_formatted},{decimal_formatted}'
