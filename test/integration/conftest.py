6a0ef27ce86dfeed89b243024e807975bff74718
Ivar Carrera <ivar.carrera@outlook.com>
2023-06-15 13:56:22 -0300

    test: Update conftest file to run with LifespanManager

--- test/integration/conftest.py
+++ test/integration/conftest.py
@@ -1,10 +1,17 @@
+import os
 from collections.abc import AsyncGenerator
 
+from asgi_lifespan import LifespanManager
 from httpx import AsyncClient
 from pytest import fixture
+
+os.environ['ENV'] = 'testing'
+
+from src.main_api import app
 
 
 @fixture
 async def client() -> AsyncGenerator[AsyncClient, None]:
-    async with AsyncClient(base_url='http://localhost:3004') as client:
-        yield client
+    async with LifespanManager(app) as manager:
+        async with AsyncClient(app=manager.app, base_url='http://test') as client:
+            yield client
