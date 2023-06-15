a182f4ba26e240895522c9143c22f921d8f381ed
Ivar Carrera <ivar.carrera@outlook.com>
2023-06-15 14:21:51 -0300

    refactor: Add condicional to linten API

--- src/main_api.py
+++ src/main_api.py
@@ -4,6 +4,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 sys.path.append(str(BASE_DIR))
 
+from src import config
 from src.application.use_case.convert_currency import ConvertCurrency
 from src.application.use_case.get_currency import GetCurrency
 from src.infrastructure.api.fastapi_adapter import FastAPIAdapter
@@ -12,17 +13,13 @@ from src.infrastructure.database.asyncpg_adapter import AsyncPGAdapter
 from src.infrastructure.repository.currency_repository_database import CurrencyRepositoryDatabase
 from src.infrastructure.repository.exchange_rate_repository_database import ExchangeRateRepositoryDatabase
 
-
-def main() -> None:
-    connection = AsyncPGAdapter()
-    currency_repository = CurrencyRepositoryDatabase(connection)
-    exchange_rate_repository = ExchangeRateRepositoryDatabase(connection)
-    convert_currency = ConvertCurrency(currency_repository, exchange_rate_repository)
-    get_currency = GetCurrency(currency_repository)
-    http_server = FastAPIAdapter()
-    RouterController(http_server, convert_currency, get_currency)
-    http_server.listen(3004)
-
-
-if __name__ == '__main__':
-    main()  # pragma: no cover
+connection = AsyncPGAdapter()
+currency_repository = CurrencyRepositoryDatabase(connection)
+exchange_rate_repository = ExchangeRateRepositoryDatabase(connection)
+convert_currency = ConvertCurrency(currency_repository, exchange_rate_repository)
+get_currency = GetCurrency(currency_repository)
+http_server = FastAPIAdapter(config.DEBUG)
+RouterController(http_server, convert_currency, get_currency)
+app = http_server.app
+if not config.TESTING:
+    http_server.listen(3005)
