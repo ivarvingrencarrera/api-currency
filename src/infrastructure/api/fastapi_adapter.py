8957e34acb66e0d92a50051c215812bdf057e979
Ivar Carrera <ivar.carrera@outlook.com>
2023-06-15 13:54:37 -0300

    refactor: Remove self.router

--- src/infrastructure/api/fastapi_adapter.py
+++ src/infrastructure/api/fastapi_adapter.py
@@ -1,21 +1,20 @@
 from collections.abc import Callable
+from typing import Any
 
 import uvicorn
 import uvloop
 from fastapi import FastAPI, HTTPException, Request
 from fastapi.responses import ORJSONResponse
-from fastapi.routing import APIRouter
 
 from src.infrastructure.api.http_server import HttpServer
 
 
 class FastAPIAdapter(HttpServer):
     def __init__(self) -> None:
-        self.app = FastAPI(default_response_class=ORJSONResponse)
-        self.router = APIRouter()
+        self.app = FastAPI(title='API Currency', default_response_class=ORJSONResponse)
 
     def on(self, method: str, url: str, callback: Callable) -> None:
-        async def route_handler(request: Request) -> dict:
+        async def route_handler(request: Request) -> Any:
             try:
                 params = request.path_params
                 body = {} if request.method == 'GET' else await request.json()
@@ -23,9 +22,8 @@ class FastAPIAdapter(HttpServer):
             except ValueError as e:
                 raise HTTPException(status_code=422, detail=str(e)) from e
 
-        self.router.add_api_route(url, route_handler, methods=[method])
+        self.app.add_api_route(url, route_handler, methods=[method])
 
     def listen(self, port: int) -> None:
-        self.app.include_router(self.router)
         uvloop.install()
         uvicorn.run(self.app, host='127.0.0.1', port=port, loop='uvloop')
